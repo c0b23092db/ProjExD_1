186bfb5e3bbec6d2c9758ba467d8faeaf336ee9f
c0b23092 <c0b23092db@edu.teu.ac.jp>
2024-04-16 15:21:42 +0900

    練習問題2 and 変数名の修正

--- pygame_image.py
+++ pygame_image.py
@@ -2,26 +2,26 @@ import os
 import sys
 import pygame as pg
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((800, 600))
+    background = pg.display.set_mode((800, 600))
+    background_img = pg.image.load("fig/pg_bg.jpg")
+    player = pg.Surface((25,25))
+    player_img = pg.image.load("fig/3.png")
+    player_img = pg.transform.flip(player_img,True,False)
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        background.blit(background_img, [0, 0])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
 
-
 if __name__ == "__main__":
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
     pg.init()
     main()
     pg.quit()
