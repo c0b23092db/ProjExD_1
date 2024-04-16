4484a1391a3e7030c3db2684b5835298da58cce5
c0b23092 <c0b23092db@edu.teu.ac.jp>
2024-04-16 16:46:05 +0900

    演習問題1 and プログラムを見やすくする

--- pygame_image.py
+++ pygame_image.py
@@ -4,33 +4,45 @@ import pygame as pg
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
+    
+    # 初期化 #
     screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+        # 変数 #
     background_img = pg.image.load("fig/pg_bg.jpg")
+    background_x = 0
     player = pg.image.load("fig/3.png")
     player = pg.transform.flip(player,True,False)
     player_rect = player.get_rect()
     player_rect.center = 300,200
     tmr = 0
-    background_x = 0
-    clock  = pg.time.Clock()
+    
     while True:
+        pressed_keys =pg.key.get_pressed()
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+
         screen.blit(background_img,[background_x*-1,0])
         screen.blit(background_img,[background_x*-1+1600,0])
-        background_x = (background_x + 2) % 1600
         screen.blit(player,player_rect)
-        pressed_keys =pg.key.get_pressed()
+
+        # 変数
         if pressed_keys[pg.K_UP]:
-            player_rect.move_ip(0,-1)
+            player_rect.move_ip(0,-2)
         elif pressed_keys[pg.K_DOWN]:
-            player_rect.move_ip(0,1)
+            player_rect.move_ip(0,2)
         elif pressed_keys[pg.K_LEFT]:
-            player_rect.move_ip(-1,0)
+            player_rect.move_ip(-2,0)
         elif pressed_keys[pg.K_RIGHT]:
-            player_rect.move_ip(1,0)
-        pg.display.update()
+            player_rect.move_ip(2,0)
+            background_x += 1
+        if not pressed_keys[pg.K_LEFT]:
+            player_rect.move_ip(-1,0)
+        background_x = (background_x + 1) % 1600
         tmr += 1
+
+        # 更新
+        pg.display.update()
         clock.tick(200)
 
 if __name__ == "__main__":
